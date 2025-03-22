96996cca4a92da7970e27ae97ac049d6b0e82886
nusrat10-cell <nushratahmedmiky@gmail.com>
2025-03-22 20:09:41 +0600

    Updated homepage content and added more details

---
+++ script.js
@@ -0,0 +1,15 @@
+// Show alert when clicking 'Learn More' button
+function showAlert() {
+    alert("Welcome to Happy Skin! Your skin deserves the best.");
+}
+
+// Add product to cart (demo function)
+function addToCart() {
+    alert("Product added to cart!");
+}
+
+// Submit contact form
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us. We will get back to you soon!");
+}
